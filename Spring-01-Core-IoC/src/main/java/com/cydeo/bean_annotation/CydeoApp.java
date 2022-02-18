6be9ab86828c4b74787547a92ab5f7e9cf11734c
Abrehett <seseney2022@gmail.com>
2022-02-17 19:40:43 -0500

    stereotype annotation

--- Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
+++ Spring-01-Core-IoC/src/main/java/com/cydeo/bean_annotation/CydeoApp.java
@@ -7,10 +7,21 @@ public class CydeoApp {
     public static void main(String[] args) {
         ApplicationContext container= new AnnotationConfigApplicationContext(ConfigApp.class,ConfigAny.class);
            FullTimeMentor ft= container.getBean(FullTimeMentor.class);
+
+           // PartTimeMentor pt = container.getBean("p2",PartTimeMentor.class);
+        PartTimeMentor pt = container.getBean(PartTimeMentor.class);
+
+
+
          ft.createAccount();
+        pt.createAccount();
 
           String str= container.getBean(String.class);
         System.out.println(str);
+
+
+
+
 
     }
 }
