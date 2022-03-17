466b2bda46890d0cbe83445fcc5c441a0b96f2d0
Abrehett <seseney2022@gmail.com>
2022-03-17 13:14:04 -0400

    welcome html

--- Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/bootstrap/DataGenerator.java
+++ Spring-09-Spring-Thymeleaf/src/main/java/com/cydeo/bootstrap/DataGenerator.java
@@ -8,13 +8,13 @@ import java.util.List;
 
 public class DataGenerator {
     public static List<Student> createStudent(){
-List<Student>student= Arrays.asList(
-        new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
-        new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
-        new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
-        new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state()),
-        new Student(new Faker().name().firstName(),new Faker().name().lastName(),new Faker().number().numberBetween(20,50),new Faker().address().state())
-);
-    return student;
+        List<Student>students= Arrays.asList(
+                new Student( new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,50),new Faker().address().state()),
+                new Student( new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,50),new Faker().address().state()),
+                new Student( new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,50),new Faker().address().state()),
+                new Student( new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,50),new Faker().address().state()),
+                new Student( new Faker().name().firstName(), new Faker().name().lastName(), new Faker().number().numberBetween(20,50),new Faker().address().state())
+        );
+        return students;
     }
 }
