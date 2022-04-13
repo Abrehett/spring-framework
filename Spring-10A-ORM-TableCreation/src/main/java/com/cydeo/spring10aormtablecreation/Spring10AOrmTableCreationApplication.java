786e4748405077189272a92cb48f73843385a875
Abrehett <seseney2022@gmail.com>
2022-04-13 02:31:15 -0400

    ORM part-1

---
+++ Spring-10A-ORM-TableCreation/src/main/java/com/cydeo/spring10aormtablecreation/Spring10AOrmTableCreationApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring10aormtablecreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring10AOrmTableCreationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring10AOrmTableCreationApplication.class, args);
+    }
+
+}
---
+++ Spring-10A-ORM-TableCreation/src/test/java/com/cydeo/spring10aormtablecreation/Spring10AOrmTableCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.spring10aormtablecreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring10AOrmTableCreationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
